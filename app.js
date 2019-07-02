526a94bb38c85ca9c8bcfd5c05cac7effb68c574
rakshajag13 <raksha.jag13@gmail.com>
2019-07-02 17:31:30 +0530

    added mongo url

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const PORT=process.env.PORT ||4200;
 
 var mongoose = require('mongoose');
 var bodyParser=require('body-parser');
-var url = 'mongodb://localhost:27017/uapplication'
-mongoose.connect(url, function (err) {
+//var url = 'mongodb://localhost:27017/uapplication'
+var url = 'mongodb://uapp:uapp123@ds243717.mlab.com:43717/uapplication'
+//var url='mongodb+srv://raksha:Raksha123@cluster0-zxws0.mongodb.net/uapplication?retryWrites=true&w=majority'
+mongoose.connect(url,{useNewUrlParser:true} ,function (err) {
     if (err) {
         console.log(err)
     }
